d338d3f70a59bd9398fedd30f0420fb8eef718a1
Sohan Matheesha <105502291+Mathiya578@users.noreply.github.com>
2023-04-11 20:05:43 +0530

    ❤️⃟✦⃝̮̮̮̯̯̯😌Sohan Matheesha🌹⃝✥⃟☺️᭄ꦿ

---
+++ BotCharacters.js
@@ -0,0 +1,61 @@
+/* ---------------------------------------------------------------------------------/
+/                                                                                   /
+/             d8888 888    888                        888b     d888 8888888b.       /
+/            d88888 888    888                        8888b   d8888 888  "Y88b      /
+/           d88P888 888    888                        88888b.d88888 888    888      /
+/          d88P 888 888888 888  8888b.  .d8888b       888Y88888P888 888    888      /
+/         d88P  888 888    888     "88b 88K           888 Y888P 888 888    888      /
+/        d88P   888 888    888 .d888888 "Y8888b.      888  Y8P  888 888    888      /
+/       d8888888888 Y88b.  888 888  888      X88      888   "   888 888  .d88P      /
+/      d88P     888  "Y888 888 "Y888888  88888P'      888       888 8888888P"       /
+/                                                                                   / 
+/-----------------------------------------------------------------------------------/
+/ Author and Main Developer: FantoX                                                 /
+/ Github: https://github.com/FantoX001/Atlas-MD                                     /
+/ Powered By: Team ATLAS                                                            /
+/-----------------------------------------------------------------------------------/
+/             Meet Team ATLAS who holds all rights to this repository:              /
+/                                                                                   /
+/ 1. Pratyush - https://github.com/pratyush4932                                     /
+/ 2. Ahmii - https://github.com/Ahmii-kun                                           /               
+/ 3. Kai - https://github.com/Kai0071                                               /                    
+/ 4. Devime - https://github.com/Devime69                                           /
+/ 5. Jay JayOps - https://github.com/jayjay-ops                                     /
+/                                                                                   /
+/ ----------------------------------------------------------------------------------/
+/                                                                                   /
+/      With all of our hard work and defication you can enjoy this awesome bot!     /  
+/                                                                                   / 
+/----------------------------------------------------------------------------------*/
+
+
+
+/* ---Added Anime Character list---  /
+
+ 0 --- SOHAN BOTZ
+
+
+Image Source: https://wall.alphacoders.com/
+Image Source: https://wallpapercave.com/
+Video Host: https://graph.org/
+
+** Note: 
+Do not change or remove present charID. You can add more characters by adding more charID sequentially.
+Add character name, video link, and image links.
+Don't forget to change "charlist.js" and "setchar.js" insdide "Commands\Mods\"  folder.
+
+*/
+
+
+
+global.charID0 = {
+    botName : "SOHAN BOTZ",
+    botVideo : "https://graph.org/file/e3b2ee218b98b8679bf98.mp4",
+    botImage1 : "https://i.imgur.com/MClOeqe.jpeg",
+    botImage2 : "https://i.imgur.com/MClOeqe.jpeg",
+    botImage3 : "https://i.imgur.com/MClOeqe.jpeg",
+    botImage4 : "https://i.imgur.com/MClOeqe.jpeg",
+    botImage5 : "https://i.imgur.com/MClOeqe.jpeg",
+    botImage6 : "https://i.imgur.com/MClOeqe.jpeg",
+}
+
